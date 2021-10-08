c8a7f50cefa990a10a37ebce589a4227e3ac08ca
iscojin <flutterdev9715@gmail.com>
2021-10-08 19:10:59 +0100

    2021-10-08

--- lib/keicy_checkbox.dart
+++ lib/keicy_checkbox.dart
@@ -4,123 +4,154 @@ import 'package:flutter/material.dart';
 
 class KeicyCheckBox extends FormField<Map<String, dynamic>> {
   KeicyCheckBox({
-    Key key,
-    double width,
-    @required double iconSize,
-    bool value = false,
-    dynamic trueIcon = Icons.check_box,
-    dynamic falseIcon = Icons.check_box_outline_blank,
-    Color iconColor = Colors.blue,
-    Color trueIconColor,
-    Color falseIconColor,
-    Color disabledColor = Colors.grey,
-    String label = "",
-    TextStyle labelStyle,
-    double labelFontSize = 20,
-    Color labelColor = Colors.black,
-    double labelSpacing = 10,
-    bool autovalidate = false,
-    bool enabled = true,
-    bool readOnly = false,
-    bool fixedHeightState = false,
-    bool stateChangePossible = false,
-    Function onSaveHandler,
-    Function onChangeHandler,
-    FormFieldValidator<bool> onValidateHandler,
-    CrossAxisAlignment crossAxisAlignment = CrossAxisAlignment.center,
+    Key? key,
+    double? width,
+    bool? value = false,
+    IconData? trueIcon = Icons.check_box,
+    IconData? falseIcon = Icons.check_box_outline_blank,
+    @required double? iconSize,
+    Color? iconColor = Colors.blue,
+    Color? trueIconColor,
+    Color? falseIconColor,
+    Color? disabledColor = Colors.grey,
+    String? label = "",
+    TextStyle? labelStyle,
+    double? labelSpacing = 10,
+    bool? autovalidate = false,
+    bool? enabled = true,
+    bool? readOnly = false,
+    bool? stateChangePossible = false,
+    Function(bool)? onSaveHandler,
+    Function(bool)? onChangeHandler,
+    FormFieldValidator<bool>? onValidateHandler,
+    CrossAxisAlignment? crossAxisAlignment = CrossAxisAlignment.center,
   }) : super(
           key: key,
           initialValue: {"value": value, "oldValue": value},
-          autovalidateMode: autovalidate ? AutovalidateMode.always : AutovalidateMode.onUserInteraction,
+          autovalidateMode: autovalidate! ? AutovalidateMode.always : AutovalidateMode.onUserInteraction,
           validator: (value) {
-            if (onValidateHandler != null) return onValidateHandler(value["value"]);
+            if (onValidateHandler != null) return onValidateHandler(value!["value"]);
             return null;
           },
           onSaved: (value) {
-            if (onSaveHandler != null) onSaveHandler(value["value"]);
+            if (onSaveHandler != null) onSaveHandler(value!["value"]);
           },
           builder: (FormFieldState<Map<String, dynamic>> state) {
             trueIconColor = trueIconColor ?? iconColor;
             falseIconColor = falseIconColor ?? iconColor;
-            WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-              if (value != state.value["oldValue"]) {
+            WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
+              if (value != state.value!["oldValue"]) {
                 state.didChange({"value": value, "oldValue": value});
               }
             });
+
             return Padding(
               padding: EdgeInsets.zero,
-              child: state.value["value"]
-                  ? _createTappableIcon(state, enabled, readOnly, trueIcon, trueIconColor, disabledColor, iconSize, label, labelFontSize, labelColor,
-                      width, fixedHeightState, stateChangePossible, onChangeHandler, labelStyle, crossAxisAlignment, labelSpacing)
-                  : _createTappableIcon(state, enabled, readOnly, falseIcon, falseIconColor, disabledColor, iconSize, label, labelFontSize,
-                      labelColor, width, fixedHeightState, stateChangePossible, onChangeHandler, labelStyle, crossAxisAlignment, labelSpacing),
+              child: state.value!["value"]
+                  ? _createTappableIcon(
+                      state,
+                      enabled,
+                      readOnly,
+                      trueIcon,
+                      trueIconColor,
+                      disabledColor,
+                      iconSize,
+                      label,
+                      width,
+                      stateChangePossible,
+                      onChangeHandler,
+                      labelStyle,
+                      crossAxisAlignment,
+                      labelSpacing,
+                    )
+                  : _createTappableIcon(
+                      state,
+                      enabled,
+                      readOnly,
+                      falseIcon,
+                      falseIconColor,
+                      disabledColor,
+                      iconSize,
+                      label,
+                      width,
+                      stateChangePossible,
+                      onChangeHandler,
+                      labelStyle,
+                      crossAxisAlignment,
+                      labelSpacing,
+                    ),
             );
           },
         );
 
   static Widget _createTappableIcon(
-    FormFieldState<Map<String, dynamic>> state,
-    bool enabled,
-    bool readOnly,
-    dynamic icon,
-    Color iconColor,
-    Color disabledColor,
-    double iconSize,
-    String label,
-    double labelFontSize,
-    Color labelColor,
-    double width,
-    bool fixedHeightState,
-    bool stateChangePossible,
-    Function onChangeHandler,
-    TextStyle labelStyle,
-    CrossAxisAlignment crossAxisAlignment,
-    double labelSpacing,
+    FormFieldState<Map<String, dynamic>>? state,
+    bool? enabled,
+    bool? readOnly,
+    IconData? icon,
+    Color? iconColor,
+    Color? disabledColor,
+    double? iconSize,
+    String? label,
+    double? width,
+    bool? stateChangePossible,
+    Function(bool)? onChangeHandler,
+    TextStyle? labelStyle,
+    CrossAxisAlignment? crossAxisAlignment,
+    double? labelSpacing,
   ) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         GestureDetector(
-          onTap: (enabled && !readOnly) ? () => tapHandler(state, stateChangePossible, onChangeHandler) : null,
+          onTap: (enabled! && !readOnly!)
+              ? () {
+                  tapHandler(state, stateChangePossible, onChangeHandler);
+                }
+              : null,
           child: Container(
             width: width,
+            // height: height,
             alignment: Alignment.center,
             child: Row(
               mainAxisAlignment: (label == "") ? MainAxisAlignment.center : MainAxisAlignment.start,
-              crossAxisAlignment: crossAxisAlignment,
+              crossAxisAlignment: crossAxisAlignment!,
               children: <Widget>[
-                icon.runtimeType == IconData ? Icon(icon, color: enabled ? iconColor : disabledColor, size: iconSize) : icon,
-                (label == "") ? SizedBox() : SizedBox(width: labelSpacing),
+                Icon(icon, color: enabled ? iconColor : disabledColor, size: iconSize),
+                (label == "") ? const SizedBox() : SizedBox(width: labelSpacing),
                 (label == "")
-                    ? SizedBox()
+                    ? const SizedBox()
                     : (width == null)
-                        ? Text(label, style: labelStyle ?? TextStyle(fontSize: labelFontSize, color: labelColor))
+                        ? Text(label!, style: labelStyle)
                         : Expanded(
-                            child: Text(label, style: labelStyle ?? TextStyle(fontSize: labelFontSize, color: labelColor)),
+                            child: Text(label!, style: labelStyle),
                           )
               ],
             ),
           ),
         ),
-        (state.hasError)
+        (state!.hasError)
             ? Container(
-                height: labelFontSize + 5,
+                padding: const EdgeInsets.symmetric(vertical: 3),
                 child: Text(
                   (state.errorText ?? ""),
-                  style: TextStyle(fontSize: (labelStyle != null) ? labelStyle.fontSize * 0.8 : labelFontSize * 0.8, color: Colors.red),
+                  style: TextStyle(fontSize: (labelStyle != null) ? labelStyle.fontSize! * 0.8 : 12, color: Colors.red),
                 ),
               )
-            : (fixedHeightState)
-                ? SizedBox(height: labelFontSize + 5)
-                : SizedBox(),
+            : const SizedBox(),
       ],
     );
   }
 }
 
-void tapHandler(FormFieldState<Map<String, dynamic>> state, bool stateChangePossible, Function onChangeHandler) {
-  bool newState = !state.value["value"];
-  state.didChange({"value": newState, "oldValue": state.value["oldValue"]});
-  if (onChangeHandler != null) onChangeHandler(newState);
+void tapHandler(FormFieldState<Map<String, dynamic>>? state, bool? stateChangePossible, Function(bool)? onChangeHandler) {
+  try {
+    bool newState = !state!.value!["value"];
+    state.didChange({"value": newState, "oldValue": state.value!["oldValue"]});
+    if (onChangeHandler != null) onChangeHandler(newState);
+  } catch (e) {
+    // ignore: avoid_print
+    print(e);
+  }
 }
